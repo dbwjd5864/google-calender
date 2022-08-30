c400ad71f4c09f0f3fa3184d139ea25404ec216e
Yujeong Choung <yujeong.choung@gmail.com>
2022-08-30 23:06:49 +0900

    feat: initial setting and commit

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react', 'import', 'prettier'],
+  ignorePatterns: ['build', 'dist', 'public'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+  },
+};
