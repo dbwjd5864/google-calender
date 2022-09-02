7871a899ef8564e6063b8e2f052c5203fae34445
Yujeong Choung <yujeong.choung@gmail.com>
2022-09-02 21:22:11 +0900

    feat: sidebar 레이아웃 수정

--- src/components/Calendar/Sidebar/index.tsx
+++ src/components/Calendar/Sidebar/index.tsx
@@ -7,7 +7,7 @@ const Sidebar = () => {
   const handleEventRegisterModal = () => {};
 
   return (
-    <aside className={'h-full p-2 pt-3 basis-5/12'}>
+    <aside className={'h-full p-2 pt-3 w-80'}>
       <Button
         onClick={handleEventRegisterModal}
         className="px-3 py-2 border rounded-full flex items-center shadow transition-shadow ease-in-out hover:bg-[#f6fafe] hover:shadow-2xl hover:shadow-[1px_8px_10px_0_rgba(60,64,67,0.3)]">
