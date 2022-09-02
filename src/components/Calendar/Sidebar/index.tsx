29e26567253383fca05d0712d553993a7f03a4bd
Yujeong Choung <yujeong.choung@gmail.com>
2022-09-02 18:59:42 +0900

    feat: 사이드바 레이아웃 변경

--- src/components/Calendar/Sidebar/index.tsx
+++ src/components/Calendar/Sidebar/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Button from '../../Common/Button';
 import plus from '../../../assets/images/plus.svg';
-import DatePicker from '../DatePicker';
+import DatePicker from './DatePicker';
 
 const Sidebar = () => {
   const handleEventRegisterModal = () => {};
 
   return (
-    <aside className={'h-full p-2 pt-3 w-64'}>
+    <aside className={'h-full p-2 pt-3 basis-5/12'}>
       <Button
         onClick={handleEventRegisterModal}
         className="px-3 py-2 border rounded-full flex items-center shadow transition-shadow ease-in-out hover:bg-[#f6fafe] hover:shadow-2xl hover:shadow-[1px_8px_10px_0_rgba(60,64,67,0.3)]">
