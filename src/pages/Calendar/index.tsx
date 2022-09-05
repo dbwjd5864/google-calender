18387cd6c0f6db0c45d1b9f52cb0b820d7be1a31
Yujeong Choung <yujeong.choung@gmail.com>
2022-09-05 14:13:15 +0900

    fix: 이벤트 등록 모달 생성

--- src/pages/Calendar/index.tsx
+++ src/pages/Calendar/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../../components/Calendar/Header';
 import Sidebar from '../../components/Calendar/Sidebar';
 import WeeklyCalendar from '../../components/Calendar/WeeklyCalendar';
+import EventModal from '../../components/Calendar/EventModal';
 
 const Calendar = () => {
   return (
@@ -10,6 +11,7 @@ const Calendar = () => {
       <main className={'flex flex-1'}>
         <Sidebar />
         <WeeklyCalendar />
+        <EventModal />
       </main>
     </div>
   );
